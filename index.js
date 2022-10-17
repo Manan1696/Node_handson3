aa52cd795a3d69d0779f0cc4ac64bc4951d616bc
manvendra <manvendrachauhan535@gmail.com>
2022-10-18 04:01:15 +0530

    new

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const middleWare1 = (req,res,next) => {
     console.log("First Middleware");
     next();
-    
+
 }
 const middleWare2 = (req,res,next) => {
     console.log("Second Middleware");
@@ -21,15 +21,15 @@ app.get('/', (req,res)=>{
     res.send('Main Page');
 })
 
-app.get('/home', (req,res)=>{
+app.get('/day1', (req,res)=>{
     res.send('Day1 React'); 
 })
 
-app.get('/about',middleWare2, (req,res)=>{
+app.get('/day2',middleWare2, (req,res)=>{
     res.send('<h1>Day2 React</h1>');
 })
 
-app.get('/contact',middleWare2, (req,res)=>{
+app.get('/final',middleWare2, (req,res)=>{
     res.send('<h1>Final Day</h1>');
 })
 
